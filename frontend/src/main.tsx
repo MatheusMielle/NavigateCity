0662431a34a87960078073aa87634a26f1fec788
Matheus Silva <msilva25@my.whitworth.edu>
2025-08-04 01:41:25 -0700

    /navigatecity as base

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,23 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
-import Home from './pages/Home.tsx'
-import Explore from './pages/Explore.tsx'
-import Results from './pages/Results.tsx';
+import Home from "./pages/Home.tsx";
+import Explore from "./pages/Explore.tsx";
+import Results from "./pages/Results.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/explore" element={<Explore />} />
-        <Route path="/results" element={<Results />} />
-      </Routes>
-    </BrowserRouter>
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter basename="/navigatecity">
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/explore" element={<Explore />} />
+      <Route path="/results" element={<Results />} />
+    </Routes>
+  </BrowserRouter>
+);
