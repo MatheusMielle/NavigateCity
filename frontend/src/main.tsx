0d8f0f83f87579f1e0c24aaed31bf10374d61dff
Matheus Silva <msilva25@my.whitworth.edu>
2025-08-04 01:18:01 -0700

    d

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
 import Home from './pages/Home.tsx'
 import Explore from './pages/Explore.tsx'
 import Results from './pages/Results.tsx';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
